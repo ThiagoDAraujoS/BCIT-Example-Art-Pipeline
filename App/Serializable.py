255de11e3f25ba6e6421d6bb004fe60f03c06c19
Thiago de Araujo Silva <ThiagoDAraujoS@Gmail.com>
2023-06-28 02:14:09 -0700

    Added Specialized time date Type serialization to Serializable.py
    Serializable.py now is able to serialize time and date primitives

--- App/Serializable.py
+++ App/Serializable.py
@@ -1,7 +1,7 @@
 import json
 from os import path
 
-FILE_NAME: str = "meta.data"
+FILE_NAME: str = "data.meta"
 FILE_HEADER: str = "FILE CREATED BY: THIAGO dA. SILVA\nBCIT - Britsh Columbia Institute of Technology\nTechnical Arts Advanced Course\n"
 FILE_DATA_BULLET: str = "DATA>>>\n"
 
@@ -9,10 +9,13 @@ def serializable(cls):
     """
     Decorator that adds serialization and deserialization methods to a class.
 
-    The decorator adds the following methods to the decorated class:
+    The decorator will not serialize private _members
+    The decorator adds the following variables and methods to the decorated class:
+    - self.path: the folder path for the serialized file
     - serialize(): Serializes the object's variables and saves them into a file.
     - deserialize(folder_path: str): Loads the serialized data from a file and creates an object of the decorated class.
     """
+
     class SerializableClass(cls):
         def serialize(self) -> None:
             """
@@ -21,8 +24,17 @@ def serializable(cls):
             The object's variables are converted to JSON format and saved into a file.
             The file is created or overwritten with the serialized data, including the header text.
             """
+
             file_path = path.join(self.path, FILE_NAME)
-            json_string = json.dumps(self.__dict__, indent=4)
+
+            data = {}
+            for key, value in self.__dict__.items():
+                if isinstance(value, (date, time)):
+                    value = value.isoformat()
+
+                data[key] = value
+
+            json_string = json.dumps(data, indent=4)
             with open(file_path, "w") as file:
                 file.write(f"{FILE_HEADER}{FILE_DATA_BULLET}{json_string}")
 
@@ -44,11 +56,22 @@ def serializable(cls):
             - FileNotFoundError: If the specified file cannot be found.
             - json.JSONDecodeError: If the serialized JSON data is not valid.
             """
+
             file_path = path.join(folder_path, FILE_NAME)
+
             with open(file_path, "r") as file:
                 file_string = file.read()
+
             json_string = file_string.split(FILE_DATA_BULLET)[-1]
+
             data = json.loads(json_string)
+            for key, value in data.items():
+                try: data[key] = date.fromisoformat(value)
+                except ValueError: pass
+
+                try: data[key] = time.fromisoformat(value)
+                except ValueError: pass
+
             obj = cls()
             obj.path = folder_path
             obj.__dict__.update(data)
