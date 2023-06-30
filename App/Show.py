e8fde820cf4d682eb9ad62620eca3716448c2c4b
Thiago de Araujo Silva <ThiagoDAraujoS@Gmail.com>
2023-06-29 19:32:29 -0700

    Added Show.py and Class Show
    Added the class that defines a show.
    It contains some show info variables.
    It is a serializable.

--- App/Show.py
+++ App/Show.py
@@ -1,5 +1,24 @@
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
+from Serializable import serializable
+from datetime import date, time
+from Episode import Episode
 
+@serializable
 class Show:
-    pass
+    """ A class representing a show """
+    def __init__(self):
+        self.name: str = ""
+        self.rating: int = 0
+        self.popularity_score: int = 0
+        self.type: str = "tv_series"
+        self.cast: set[str] = set()
+        self.third_party_services: set[str] = set()
+        self.description: str = ""
+        self.certification: dict[str, str] = {}
+        self.release: str = date(1, 1, 1)
+        self.countries_of_origin: set[str] = set()
+
+        self._episodes: set[Episode] = set()
+
+    def get_episode_count(self):
+        """ Returns the number of episodes in the show """
+        return len(self._episodes)
