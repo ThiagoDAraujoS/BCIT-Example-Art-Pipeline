76b662e923326ba9464cd0a47bace0a90451ac82
Thiago de Araujo Silva <ThiagoDAraujoS@Gmail.com>
2023-07-07 14:38:23 -0700

    Added Proxy.py
    this small script file will just forward the exercise's required api calls.
    I intend to try and build a flask server on top of this file, that's why I implemented it using an imperative programing style instead of regular oop

---
+++ App/ShowManager/Proxy.py
@@ -0,0 +1,37 @@
+from .Manager import Manager
+
+
+def get_show_list():
+    pass
+
+
+def create_show(show_name: str):
+    pass
+
+
+def delete_show(show_name: str):
+    pass
+
+
+def set_show_data(show_name: str, data: dict):
+    pass
+
+
+def read_shots(show_name: str):
+    pass
+
+
+def create_shot(show_name: str, shot_name: str):
+    pass
+
+
+def get_shot_data(show_name: str, shot_name: str):
+    pass
+
+
+def delete_shot(show_name: str, shot_name: str):
+    pass
+
+
+def set_shot_data(show_name: str, shot_name: str, shot_data: dict):
+    pass
