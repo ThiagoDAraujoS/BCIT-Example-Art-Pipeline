f436ac91b54e4764a8b3800a64fee74e62df3e53
Thiago de Araujo Silva <ThiagoDAraujoS@Gmail.com>
2023-07-08 03:25:23 -0700

    Added Recursive Loading to SerializableDict.py and documentation
    Also removed unnecessary variables and method calls from shot.py

--- App/ShowManager/Shot.py
+++ App/ShowManager/Shot.py
@@ -11,15 +11,7 @@ This file contains serialized shot information."""
 @serializable(FILE_HEADER, FILE_NAME)
 class Shot:
     def __init__(self):
-        self.folder: str = ""
-        self.meta_file: str = ""
         self.clip_number: int
         self.length: time
         self.characters: set[str] = set()
         self.environments: set[str] = set()
-
-    def load(self, file):
-        pass
-
-    def save(self, file):
-        pass
