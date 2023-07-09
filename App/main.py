f290f516a7dc5259b96d7a8d809854384779bb15
Thiago de Araujo Silva <ThiagoDAraujoS@Gmail.com>
2023-07-09 02:28:49 -0700

    Deleted dead code from main.py

--- App/main.py
+++ App/main.py
@@ -2,18 +2,3 @@ from App.ShowManager.Proxy import *
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host="localhost")
-
-
-#
-#from App.ShowManager import Manager, Show
-#import os.path as path
-#
-#
-#if __name__ == '__main__':
-#    FOLDER = path.normpath('C:\\Users\\Thiago\\Desktop\\Bcit Projects\\Pipeline\\Example\\CompanyName')
-#    manager = Manager.Manager()
-#    manager.install(FOLDER)
-#    manager.create_element("Super Raptors")
-#    manager = Manager.Manager()
-#    manager.load_folder(FOLDER)
-#
