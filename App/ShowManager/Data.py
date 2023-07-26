00bf1c2f9b631fd80ed4d9e4a60a37698688a980
Thiago de Araujo Silva <ThiagoDAraujoS@Gmail.com>
2023-07-26 14:23:40 -0700

    Reduced the show and shot data descriptions into a single file.

--- App/ShowManager/Data.py
+++ App/ShowManager/Data.py
@@ -1,32 +1,26 @@
-from datetime import date, time, datetime
+from datetime import datetime, date, time
+from typing import Set, Dict
 from dataclasses import dataclass, field
-from typing import Dict
-from dataclasses_json import dataclass_json, LetterCase, Undefined
+from dataclasses_json import dataclass_json  # LetterCase, Undefined
 
 
+@dataclass_json
 @dataclass
 class Shot:
     clip_number: int = -1
     length: time = field(default_factory=lambda: datetime.now().time())
-    characters: set[str] = field(default_factory=set)
-    environments: set[str] = field(default_factory=set)
+    characters: Set[str] = field(default_factory=set)
+    environments: Set[str] = field(default_factory=set)
 
 
-@dataclass_json()
+@dataclass_json
 @dataclass
 class Show:
     type: str = "tv_series"
-    cast: set[str] = field(default_factory=set)
+    cast: Set[str] = field(default_factory=set)
     rating: int = 0
     release: date = field(default_factory=date.today)
     description: str = ""
-    popularity_score: int = 0
-    certification: dict[str, str] = field(default_factory=dict)
-    countries_of_origin: set[str] = field(default_factory=set)
-    third_party_services: set[str] = field(default_factory=set)
+    countries_of_origin: Set[str] = field(default_factory=set)
+    third_party_services: Set[str] = field(default_factory=set)
     shots: Dict[str, Shot] = field(default_factory=dict)
-
-
-show = Show()
-show.shots["test"] = Shot()
-
