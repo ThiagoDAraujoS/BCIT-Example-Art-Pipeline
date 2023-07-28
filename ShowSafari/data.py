c286a7df9daafe1ea6f9ea379c1e527e0887d84d
Thiago de Araujo Silva <ThiagoDAraujoS@Gmail.com>
2023-07-28 04:25:24 -0700

    Changed how assets are indexed and how UUIDs are used.
    They are transformed into strings then are used instead of being used as true uuids

--- ShowSafari/data.py
+++ ShowSafari/data.py
@@ -1,4 +1,3 @@
-from collections import UserDict
 from datetime import datetime, date, time
 from typing import Set, Dict, List
 from dataclasses_json import dataclass_json
@@ -13,22 +12,22 @@ class Asset:
 
     Attributes:
         name (str): The name of the asset.
-        connections (List[UUID]): List of UUIDs representing connections to other assets.
+        connections (List[str(UUID)]): List of UUIDs representing connections to other assets.
     """
     name: str
-    type: str
-    connections: List[UUID] = field(default_factory=list)
+    asset_type: str
+    connections: List[str] = field(default_factory=list)
 
-    def connect(self, connection_uuid: UUID):
+    def connect(self, connection_uuid: str):
         """
         Connect the asset to another asset.
 
         Args:
-            connection_uuid (UUID): The UUID of the asset to connect.
+            connection_uuid (str): The stringified UUID of the asset to connect.
         """
         self.connections.append(connection_uuid)
 
-    def disconnect(self, connection_uuid: UUID):
+    def disconnect(self, connection_uuid: str):
         """
         Disconnect the asset from another asset.
 
@@ -45,7 +44,7 @@ class Shot(Asset):
 
     Attributes:
         name (str): The name of the shot.
-        connections (List[UUID]): List of UUIDs representing connections to other assets.
+        connections (List[str(UUID)]): List of UUIDs representing connections to other assets.
         clip_number (int): The clip number of the shot (default: -1).
         length (time): The length of the shot (default: current time).
         characters (Set[str]): Set of characters in the shot (default: empty set).
@@ -64,7 +63,7 @@ class Sound(Asset):
 
     Attributes:
         name (str): The name of the sound asset.
-        connections (List[UUID]): List of UUIDs representing connections to other assets.
+        connections (List[str(UUID)]): List of UUIDs representing connections to other assets.
         duration (float): The duration of the sound in seconds.
         format (str): The audio format of the sound (e.g., mp3, wav, etc.).
         bitrate (int): The audio bitrate in kbps.
@@ -81,7 +80,7 @@ class Model(Asset):
 
     Attributes:
         name (str): The name of the 3D model asset.
-        connections (List[UUID]): List of UUIDs representing connections to other assets.
+        connections (List[str(UUID)]): List of UUIDs representing connections to other assets.
         vertices (int): The number of vertices in the model.
         faces (int): The number of faces (polygons) in the model.
     """
@@ -104,37 +103,14 @@ class Show:
         third_party_services (Set[str]): Set of third-party services associated with the show (default: empty set).
         shots (Dict[str, Shot]): Dictionary of shots in the show, with shot names as keys (default: empty dictionary).
     """
-    type: str = "tv series"
+    type: str = "Tv Series"
     cast: Set[str] = field(default_factory=set)
     rating: int = 0
     release: date = field(default_factory=date.today)
     description: str = ""
     countries_of_origin: Set[str] = field(default_factory=set)
     third_party_services: Set[str] = field(default_factory=set)
-    shots: List[UUID] = field(default_factory=dict)
-
-
-@dataclass_json
-@dataclass
-class AssetDictionary(UserDict):
-    """ Represents a library of assets.
-
-        Inherits from collections.UserDict.
-
-    Attributes:
-        data (dict): The dictionary containing assets with UUID keys and Asset values (default: empty dictionary).
-    """
-    data: Dict[UUID, Asset] = field(default_factory=dict)
-
-    def __setitem__(self, key, value):
-        """ __setitem__(key, value): Override of the base UserDict's __setitem__ method to enforce type constraints. """
-        if not isinstance(key, UUID):
-            raise TypeError(f"Keys must be of type UUID, got {type(key)} instead.")
-
-        if not isinstance(value, Asset):
-            raise TypeError(f"Values must be of type Asset, got {type(value)} instead.")
-
-        super().__setitem__(key, value)
+    shots: List[str] = field(default_factory=dict)
 
 
 ASSET_TYPES = {
