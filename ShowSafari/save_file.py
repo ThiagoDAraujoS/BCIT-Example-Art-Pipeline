74334cf22febf35451cf74ff46a613cde0dc2f36
Thiago de Araujo Silva <ThiagoDAraujoS@Gmail.com>
2023-07-28 04:31:05 -0700

    Added create and exists to the save file and made fields private

--- ShowSafari/save_file.py
+++ ShowSafari/save_file.py
@@ -1,42 +1,91 @@
+from ShowSafari import *
 import os.path
 from typing import Type
 from dataclasses_json import dataclass_json
 
-from .folder import Folder
-
 
 class SaveFile:
-    def __init__(self, folder, file_name):
+    """
+    A class representing a save file for serializing and deserializing data.
+
+    Attributes:
+        _folder (Folder): The folder in which the save file is located.
+        _file_path (str): The full path to the save file.
+
+    Methods:
+        __init__(self, folder: Folder, file_name: str)
+            Initializes the SaveFile with a specified folder and file name.
+
+        serialize(self, data_content: dataclass_json) -> None
+            Serializes the given data_content and writes it to the save file.
+
+        deserialize(self, data_type: Type[dataclass_json]) -> dataclass_json
+            Deserializes the data from the save file into the specified dataclass type.
+
+        create(self) -> None
+            Creates an empty save file.
+
+        exists(self) -> bool
+            Checks if the save file exists.
+
+    Example:
+        folder = Folder('/path/to/save/')
+        save_file = SaveFile(folder, 'data')
+        data = MyDataClass(some_field=42)
+        save_file.serialize(data)
+        loaded_data = save_file.deserialize(MyDataClass)
+        if not save_file.exists():
+            save_file.create()
+    """
+    EMPTY_FILE_STRING = "{}"
+
+    def __init__(self, folder: Folder, data_type: Type[dataclass_json], file_name: str = "file"):
         """
-        Initialize a SaveFile instance.
+        Initializes the SaveFile with a specified folder and file name.
 
         Args:
-            folder (Folder): The folder where the SaveFile will be stored.
-            file_name (str): The name of the SaveFile.
+            folder (Folder): The folder where the save file should be located.
+            data_type (Type[dataclass_json]): The type of data saved in this file
+            file_name (str): The name of the save file (excluding the ".json" extension).
         """
-        self.folder: Folder = folder
-        self.file_path: str = os.path.join(self.folder.path, file_name)
+        self._data_type: Type[dataclass_json] = data_type
+        self._folder: Folder = folder
+        self._file_path: str = os.path.join(self._folder.path, f"{file_name}.json")
 
-    def serialize(self, data_content: dataclass_json):
+    def serialize(self, data_content: dataclass_json) -> None:
         """
-        Serialize the data content and save it to the SaveFile.
+        Serializes the given data_content and writes it to the save file.
 
         Args:
-            data_content (dataclass_json): The data content to be serialized.
+            data_content (dataclass_json): The data to be serialized and saved into the file.
         """
-        with open(self.file_path, 'w') as file:
+        with open(self._file_path, 'w') as file:
             file.write(data_content.to_json())
 
-    def deserialize(self, data_type: Type[dataclass_json]) -> dataclass_json:
+    def deserialize(self) -> dataclass_json:
         """
-        Deserialize the data from the SaveFile into the specified dataclass type.
-
-        Args:
-            data_type (Type[dataclass_json]): The type of the dataclass to deserialize to.
+        Deserializes the data from the save file into the specified dataclass type.
 
         Returns:
             dataclass_json: The deserialized data of the specified dataclass type.
         """
-        with open(self.file_path, 'r') as file:
+        with open(self._file_path, 'r') as file:
             data_string = file.read()
-        return data_type.from_json(data_string)
+        return self._data_type.from_json(data_string)
+
+    def create(self) -> None:
+        """
+        Creates an empty file.
+        """
+        with open(self._file_path, 'w') as file:
+            data_string = SaveFile.EMPTY_FILE_STRING
+            file.write(data_string)
+
+    def exists(self) -> bool:
+        """
+        Checks if the save file exists.
+
+        Returns:
+            bool: True if the save file exists, False otherwise.
+        """
+        return os.path.exists(self._file_path)
