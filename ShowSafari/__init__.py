31820a8a003a98f218448c463950e9a3e6a15a76
Thiago de Araujo Silva <ThiagoDAraujoS@Gmail.com>
2023-07-28 13:15:00 -0700

    Added all packages imports to __init__.py

--- ShowSafari/__init__.py
+++ ShowSafari/__init__.py
@@ -1,18 +1,19 @@
-import uuid
-from dataclasses_json import config
+from .data import Asset, Shot, Sound, Model, Show, ASSET_TYPES
+from .folder import Folder
+from .instance import Instance
+from .library import Library
+from .save_file import SaveFile
 
 
-def uuid_encoder(obj):
-    if isinstance(obj, uuid.UUID):
-        return str(obj)
-    raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
-
-
-def uuid_decoder(obj):
-    if isinstance(obj, str):
-        return uuid.UUID(obj)
-    return obj
-
-
-# Set up the custom encoder and decoder globally for dataclasses_json
-config(encoder=uuid_encoder, decoder=uuid_decoder)
+__all__ = [
+    "Asset",
+    "Shot",
+    "Sound",
+    "Model",
+    "Show",
+    "ASSET_TYPES",
+    "Folder",
+    "Instance",
+    "Library",
+    "SaveFile"
+]
