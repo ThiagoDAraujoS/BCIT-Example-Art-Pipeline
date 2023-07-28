7abd9a8221e2410c843fab72e34313d1e61a6c4f
Thiago de Araujo Silva <ThiagoDAraujoS@Gmail.com>
2023-07-28 16:00:47 -0700

    Ensure all objects used by save_file are decorated by @dataclass_json, this reduces the complexity of the code

--- ShowSafari/save_file.py
+++ ShowSafari/save_file.py
@@ -1,43 +1,30 @@
-from ShowSafari import *
+from .folder import Folder
 import os.path
-from typing import Type
 from dataclasses_json import dataclass_json
 
 
 class SaveFile:
-    EMPTY_FILE_STRING = "{}"
-
     def __init__(self, folder: Folder, data_reference: dataclass_json, file_name: str = "file"):
         self._data_reference = data_reference
         """ data_reference can only be an object or an dictionary """
         self._folder: Folder = folder
         self._file_path: str = os.path.join(self._folder.path, f"{file_name}.json")
+        self.create()
 
     def save(self) -> None:
+        json_string = self._data_reference.to_json()
         with open(self._file_path, 'w') as file:
-            file.write(self._data_reference.to_json())
+            file.write(json_string)
 
     def load(self):
         with open(self._file_path, 'r') as file:
             data_string = file.read()
-
-        loaded_data = type(self._data_reference).from_json(data_string)
-
-        if hasattr(self._data_reference, "__dict__"):
-            self._data_reference.__dict__.update(loaded_data.__dict__)
-
-        elif isinstance(self._data_reference, list):
-            self._data_reference[:] = loaded_data
-
-        else:
-            self._data_reference.update(loaded_data)
+        self._data_reference.from_json(data_string)
 
     def create(self) -> bool:
         if self.exists():
             return False
-        with open(self._file_path, 'w') as file:
-            data_string = SaveFile.EMPTY_FILE_STRING
-            file.write(data_string)
+        self.save()
         return True
 
     def exists(self) -> bool:
