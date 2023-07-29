69490e58974829c767eab957117ecb07f447b4cc
Thiago de Araujo Silva <ThiagoDAraujoS@Gmail.com>
2023-07-29 02:09:21 -0700

    Added type index tracking to the libraryData
    now it has a double dictionary, one that keep assets by uuids and another that organize uuids by type

--- ShowSafari/library.py
+++ ShowSafari/library.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from .folder import Folder
 from .save_file import SaveFile
-from .data import Asset, ASSET_TYPES
+from .data import *
 
 from dataclasses import dataclass, field
 from dataclasses_json import dataclass_json
@@ -10,21 +10,39 @@ from typing import Dict
 from uuid import uuid4 as generate_uuid
 
 
+ASSET_TYPES = {
+    'Shot': Shot,
+    'Sound': Sound,
+    'Model': Model,
+}
+
+
 @dataclass_json
 @dataclass
 class LibraryData:
-    assets: Dict[str, Asset] = field(default_factory=dict)
+    data: Dict[str, Asset] = field(default_factory=dict)
+    type_index: Dict[str, Set[str]] = field(default_factory=dict)
+
+    def add(self, asset, uuid):
+        self.data[uuid] = asset
+        self.type_index.setdefault(asset.asset_type, set()).add(uuid)
+
+    def remove(self, uuid):
+        asset = self.data.pop(uuid)
+        if asset:
+            self.type_index[asset.asset_type].remove(uuid)
 
 
 class Library:
     def __init__(self, location_path):
         self._folder: Folder = Folder(location_path, "Library")
-        self._data: LibraryData = LibraryData()
-        self._save_file: SaveFile = SaveFile(self._folder, self._data, "Library")
+        self._assets: LibraryData = LibraryData()
+        self._save_file: SaveFile = SaveFile(self._folder, self._assets, "Library")
 
         self.save = self._save_file.save
         self.load = self._save_file.load
-        self.get = self._data.assets.get
+        self.get = self._assets.data.get
+        self.get_by_name = self._assets.get_by_name
         self._save_file.create()
         self.load()
 
@@ -34,7 +52,8 @@ class Library:
         asset_type = asset_type.capitalize()
         asset_name = asset_name.capitalize()
 
-        self._data.assets[uuid] = ASSET_TYPES.get(asset_type, Asset)(asset_name, asset_type)
+        new_asset = ASSET_TYPES.get(asset_type, Asset)(asset_name, asset_type)
+        self._assets.add(new_asset, uuid)
         self._folder.create_subfolder(uuid)
         self._folder.open_folder_in_explorer(uuid)
         self.save()
@@ -43,21 +62,24 @@ class Library:
     def remove(self, asset_uuid: str):
         self._folder.delete_subfolder(asset_uuid)
         self.save()
-        self._data.assets.pop(asset_uuid)
-
-    def get_by_name(self, asset_name: str) -> str | None:
-        for uuid, asset in self._data.assets.items():
-            if asset.name == asset_name:
-                return uuid
-        return None
+        self._assets.remove(asset_uuid)
 
     def archive(self, asset_uuid: str):
         # TODO ZIP A FOLDER I MIGHT IMPLEMENT THIS ON FOLDER
         self.remove(asset_uuid)
         self.save()
 
+    def get_by_name(self, asset_name):
+        for uuid, asset in self._assets.data.items():
+            if asset.name == asset_name:
+                return uuid
+        return None
+
+    def get_types(self):
+        return list(self._assets.type_index.keys())
+
     def connect_asset(self, parent_asset: str, child_asset: str):
-        self._data.assets[parent_asset].connect(child_asset)
+        self._assets.data[parent_asset].connect(child_asset)
 
     def disconnect_asset(self, parent_asset: str, child_asset: str):
-        self._data.assets[parent_asset].disconnect(child_asset)
+        self._assets.data[parent_asset].disconnect(child_asset)
