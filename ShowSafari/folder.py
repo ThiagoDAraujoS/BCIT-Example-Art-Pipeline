65ab793632307c8e33c0e987a069bdc6bd4610e7
Thiago Silva <thiagodaraujos@gmail.com>
2023-07-29 17:27:32 -0700

    Changed create naming to setup, and ensure these methods to only create folder if there is no previously existing ones, also update documentation

--- ShowSafari/folder.py
+++ ShowSafari/folder.py
@@ -4,16 +4,14 @@ import shutil
 
 
 class Folder:
-    """
-    A class representing a folder on the file system.
-    """
     def __init__(self, location_path: str, folder_name: str):
         """
-        Initialize a Folder instance. And make a folder for it
+        Initializes a Folder object representing a directory on the file system.
+        Then create the folder if it needs to be created.
 
-        Args:
-            location_path (str): The path to the folder location.
-            folder_name (str): The folder name.
+        Parameters:
+            location_path (str): The parent directory where the folder will be created.
+            folder_name (str): The name of the folder to be created.
         """
         self._name: str = folder_name
         """_name (str): The folder name"""
@@ -21,34 +19,34 @@ class Folder:
         self._path: str = os.path.join(location_path, folder_name)
         """_path (str): The path to the folder."""
 
-        self.create()
+        self.setup()
 
     @property
     def path(self) -> str:
         """
-        Get the path of the folder.
+        Get the full path to the folder.
 
         Returns:
-            str: The folder path.
+            str: The full path to the folder.
         """
         return self._path
 
     @path.setter
     def path(self, folder_path: str):
         """
-        Set the path of the folder.
+        Set the full path to the folder.
 
-        Args:
-            folder_path (str): The new folder path.
+        Parameters:
+            folder_path (str): The new full path to the folder.
         """
         self._path: str = os.path.normpath(folder_path)
 
-    def create(self) -> bool:
+    def setup(self) -> bool:
         """
         Create the folder if it does not exist.
 
         Returns:
-            bool: True if the folder was created, False if it already exists.
+            bool: True if the folder was created, False if it already existed.
         """
         if self.exists():
             return False
@@ -63,10 +61,10 @@ class Folder:
 
     def open_folder_in_explorer(self, subfolder: str = ""):
         """
-        Open the folder (or a subfolder) in the file explorer.
+        Open the folder (or subfolder) in the file explorer.
 
-        Args:
-            subfolder (str, optional): The name of the subfolder to open. Defaults to "".
+        Parameters:
+            subfolder (str, optional): The name of the subfolder to open. Defaults to the main folder.
         """
         subfolder = self.path if not subfolder else self.get_subfolder_path(subfolder)
 
@@ -87,32 +85,38 @@ class Folder:
         """
         return os.path.exists(self._path)
 
-    def create_subfolder(self, subfolder_name: str):
+    def setup_subfolder(self, subfolder_name: str):
         """
-        Create a subfolder inside the main folder.
+        Create a subfolder within the main folder.
 
-        Args:
+        Parameters:
             subfolder_name (str): The name of the subfolder to create.
+
+        Returns:
+            bool: True if the subfolder was created, False if it already existed.
         """
+        if self.subfolder_exists(subfolder_name):
+            return False
         os.mkdir(self.get_subfolder_path(subfolder_name))
+        return True
 
     def get_subfolder_path(self, subfolder_name: str) -> str:
         """
-        Get the path of a subfolder inside the main folder.
+        Get the full path to a subfolder within the main folder.
 
-        Args:
+        Parameters:
             subfolder_name (str): The name of the subfolder.
 
         Returns:
-            str: The path of the subfolder.
+            str: The full path to the subfolder.
         """
         return os.path.join(self.path, subfolder_name)
 
     def delete_subfolder(self, subfolder_name: str):
         """
-        Delete a subfolder inside the main folder and all its contents.
+        Delete a subfolder within the main folder.
 
-        Args:
+        Parameters:
             subfolder_name (str): The name of the subfolder to delete.
         """
         subfolder_path = self.get_subfolder_path(subfolder_name)
@@ -120,9 +124,9 @@ class Folder:
 
     def subfolder_exists(self, subfolder_name: str) -> bool:
         """
-        Check if a subfolder exists inside the main folder.
+        Check if a subfolder exists within the main folder.
 
-        Args:
+        Parameters:
             subfolder_name (str): The name of the subfolder.
 
         Returns:
@@ -132,19 +136,7 @@ class Folder:
         return os.path.exists(subfolder_path)
 
     def __repr__(self) -> str:
-        """
-        Get the string representation of the Folder object.
-
-        Returns:
-            str: The folder path.
-        """
         return self.path
 
     def __str__(self) -> str:
-        """
-        Get the string representation of the Folder object.
-
-        Returns:
-            str: The folder path.
-        """
         return f"Folder path: {self.path}"
