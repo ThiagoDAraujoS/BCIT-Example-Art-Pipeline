7204b02bb24fdc141ea9730453c827419e85e5a6
Thiago de Araujo Silva <ThiagoDAraujoS@Gmail.com>
2023-08-04 20:26:58 -0700

    Added custom errors and specialized string types to the package root.
    Since I am dealing with several different types of strings, [UUID as strings, file paths, json strings, asset type names] I am implementing annotation variations to these string types, in order to display more accurately what kind of string the methods are using and returning.
    
    Also, some new recurrent error types have been established here.

--- ShowSafari/__init__.py
+++ ShowSafari/__init__.py
@@ -1,4 +1,6 @@
 from datetime import date, time, datetime
+from typing import NewType, Type
+
 import dataclasses_json.cfg
 
 dataclasses_json.cfg.global_config.encoders[date] = date.isoformat
@@ -9,3 +11,32 @@ dataclasses_json.cfg.global_config.decoders[time] = time.fromisoformat
 
 dataclasses_json.cfg.global_config.encoders[datetime] = datetime.isoformat
 dataclasses_json.cfg.global_config.decoders[datetime] = datetime.fromisoformat
+
+UUIDString = NewType('UUIDString', str)
+JsonString = NewType('JsonString', str)
+TypeString = NewType('TypeString', str)
+PathString = NewType('PathString', str)
+
+IGNORE_ERRORS = False
+
+
+def ERROR(error_type: Type[Exception], message: str):
+    if IGNORE_ERRORS:
+        print(message)
+    else:
+        error_type(message)
+
+
+class AssetArchiveError(Exception):
+    """Exception raised when an operation is not allowed for an archived asset."""
+    pass
+
+
+class EmptyFolderError(Exception):
+    """Custom exception for handling empty folders during archiving."""
+    pass
+
+
+class ConnectToSelfError(Exception):
+    """ Custom Exception for handling assets connecting to themselves"""
+    pass
